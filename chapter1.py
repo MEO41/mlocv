ec666695bc74c89477fb2e41afaf075c6d0be401
Meo <onay41emin@gmail.com>
2022-09-10 22:17:50 +0300

    open_cam

--- chapter1.py
+++ chapter1.py
@@ -1,6 +1,3 @@
-from sre_constants import SUCCESS
-
-
 try:
     print("Packages Importing...")
     import cv2
@@ -8,14 +5,27 @@ try:
 except:
     print("Packages cannot imported")
 
-cap = cv2.VideoCapture("Resources/meVid.mp4")
+cap = cv2.VideoCapture(0)
+#define width
+cap.set(3,640)
+#define height
+cap.set(4,480)
+#change brightness
+#3, 4, 10, is id for options like brightness or height of the frame
+cap.set(10,100)
 
-while True:
-    success, img = cap.read()
-    cv2.imshow("Video",img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
+try:
+    print("cam is opening..") 
+    while True:
+        success, img = cap.read()
+   
+        
+        cv2.imshow("Video",img)
+   
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+except:
+    print("cam did not opened")
 
 
  
